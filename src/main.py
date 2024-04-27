935a72b43e2b2a5d209ca9afc7d77f83c8cce6bc
allythe <cvrurgal@yandex.ru>
2024-04-27 09:28:32 +0300

    В каждой вершине сохраняются все пути до этой вершины

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
+import itertools
 
 import pandas as pd
 import numpy as np
+from time import time
 
-from logger import logger
 class CompatabilityMatrixFinder:
     def __init__(self, input_matrix_path, weights_path, out_path):
         self.weights = None
@@ -23,53 +24,72 @@ class CompatabilityMatrixFinder:
 
         self.hyp_num = len(self.weights)
 
-        logger.info(f"Comp matrix shape : {self.comp_matrix.shape}")
-        logger.info(f"Weights number : {len(self.weights)}")
 
+    def is_compatable(self, path, e):
+        # print(path, e)
+        comp_matrix_e = self.comp_matrix[e, :]
+        comp_values = [comp_matrix_e[i] for i in path]
+        return comp_values == [1]*len(comp_values)
+
+    def delete_duplicates(self, list_paths_lengths):
+        pass
     def dijkstra_max_path(self, s):
-        # в массиве дист хранить списки путей, по которым дошли в вершину
-        dist = np.array(np.ones((1, self.hyp_num)) * -np.inf).ravel()
+        # в массиве дист хранить списки путей, по которым дошли в вершину и их длины
+        # dist[i] = [ (path1 = [1,2,3,5], len = 7), (path2, len)   ]
+        dist = [[] for i in range(self.hyp_num)]
 
-        used = np.array(np.zeros((1, self.hyp_num))).ravel()
-        print(self.weights)
-        dist[s] = self.weights[s]
+        dist[s] = [[[s], self.weights[s]]]
         for i in range(self.hyp_num):
-            print(dist)
-            v = None
-
-            # ищем неотмеченную вершину с максимальным расстоянием до нее
-            for j in range(self.hyp_num):
-                if not used[j] and (v is None or dist[j] > dist[v]):
-                    v = j
-
-            if v == None or dist[v] == -np.inf:
-                break
-            # отметили вершину посещенной
-            used[v] = 1
+            v = i
             # идем по строке в матрице совместности
-            for e in range(self.hyp_num):
-                if e>v:
-                    if self.comp_matrix[v, e] == 1:
-                        if dist[v] + self.weights[e] > dist[e]:
-                            print(v, e, dist[v], dist[e], self.weights[e])
 
-                            dist[e] = dist[v] + self.weights[e]
+            for e in range(v+1, self.hyp_num):
+                if self.comp_matrix[e, v] == 1:
+                    for path_i in range(len(dist[v])):
+                        path_length_cur = dist[v][path_i]
+                        # print(dist, dist[v], path_length_cur)
+                        path = path_length_cur[0]
+                        length = path_length_cur[1]
+
+                        if self.is_compatable(path, e):
+                            dist[e].append([path + [e], length + self.weights[e]])
+
+                        # dist[e] = self.delete_duplicates(dist[e])
+
 
         return dist
 
     def __call__(self):
-        for i in range(self.hyp_num):
-            dist = self.dijkstra_max_path(i)
-            print(dist)
-            break
+        max_len = 0
+        path_max_len = []
+
+        st = time()
+        dist = self.dijkstra_max_path(0)
+        print(f"Dijkstra run time {time()-st}")
+        dist = list(itertools.chain(*dist))
+
+        # дописать что массив dist в конце
+        # после сортируется по 2 элементу и выводятся топ - х
+        # путей с их весами
+
+        # код ниже нужен временно чтобы мы понимали верно ли работает вообще
+        for el in dist:
+            cur_path_len = el[1]
+            cur_path = el[0]
+
+            if cur_path_len > max_len:
+                max_len = cur_path_len
+                path_max_len = cur_path
+
+        print(max_len, path_max_len)
 
 
 
 
 def main():
-    input_matrix_path = "data/input_matrix2.csv"
+    input_matrix_path = "data/input_matrix.csv"
     out_path = "data/out.csv"
-    weights_path = "data/weights2.csv"
+    weights_path = "data/weights.csv"
     comp_matrix_finder = CompatabilityMatrixFinder(input_matrix_path,weights_path, out_path)
     comp_matrix_finder()
 if __name__ == "__main__":
