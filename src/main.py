27e035d11734ce0ac363b338565a30ec1d6da188
SAFUANlip <iusupov.se@phystech.edu>
2024-04-27 13:10:28 +0300

    Топ-5 предсказаний. Сохранение в датафрейм

--- src/main.py
+++ src/main.py
@@ -4,15 +4,16 @@ from time import time
 import networkx as nx
 import numpy as np
 import pandas as pd
-from networkx.algorithms.clique import max_weight_clique
+from custom_lib.custom_click_alg import custom_max_weight_clique
 
 
 class CompatabilityMatrixFinder:
-    def __init__(self, input_matrix_path, weights_path, out_path):
+    def __init__(self, input_matrix_path, weights_path, out_path, pred_path):
         self.weights = None
         self.input_df = None
         self.out_df = None
         self.hyp_num = None
+        self.pred_path = pred_path
 
         self.__read_data(input_matrix_path, weights_path, out_path)
 
@@ -34,19 +35,19 @@ class CompatabilityMatrixFinder:
         return comp_values == [1] * len(comp_values)
 
     def max_clique_solution(self):
-        multiplic = 10 ** 5
+        self.multiplic = 10 ** 5
         G = nx.Graph()
 
         for i in range(len(self.weights)):
-            G.add_node(i, weight=int(self.weights[i] * multiplic))
+            G.add_node(i, weight=int(self.weights[i] * self.multiplic))
 
         for i in range(len(self.weights)):
             for j in range(i + 1, len(self.weights)):
                 if self.comp_matrix[i, j] == 1:
                     G.add_edge(i, j)
 
-        output = max_weight_clique(G)
-        return output[0], output[1] / multiplic
+        output = custom_max_weight_clique(G)
+        return output[2]
 
     def dijkstra_solution(self, s):
         # в массиве дист хранить списки путей, по которым дошли в вершину и их длины
@@ -69,25 +70,50 @@ class CompatabilityMatrixFinder:
                             dist[e].append([path + [e], length + self.weights[e]])
         return dist
 
+    def cvt_path2gh(self, path):
+        ans = np.zeros((1, self.hyp_num), np.uint8).ravel()
+        for el in path:
+            ans[el] = int(1)
+
+        return ans
+
+    def create_pred_df(self, top5_dict):
+        hyp_names = ["TH"+ str(i+1) for i in range(self.hyp_num)]
+        pred_df = pd.DataFrame(columns=hyp_names + ["sum(w)"])
+
+        for key in top5_dict.keys():
+            cur_path = top5_dict[key]
+            cur_weight = key / self.multiplic
+            cur_path_gh = self.cvt_path2gh(cur_path)
+
+            pred_df.loc[-1] = list(cur_path_gh) + [cur_weight]  # adding a row
+            pred_df.index = pred_df.index + 1  # shifting index
+            pred_df = pred_df.sort_index()
+
+        pred_df[hyp_names] = pred_df[hyp_names].astype(int)
+        pred_df.to_csv(self.pred_path, index=False)
+
     def __call__(self):
         st = time()
-        ans = np.zeros((1, self.hyp_num), np.uint8).ravel()
 
-        path, weight = self.max_clique_solution()
-
-        for el in path:
-            ans[el] = 1
-
-        path.sort()
-        print(ans, path, weight)
+        top5_dict = self.max_clique_solution()
+        top5_dict = dict(sorted(top5_dict.items(), reverse=True))
         print(f"Решение отработало за {time() - st} секунд")
+        st = time()
+
+        self.create_pred_df(top5_dict)
+
+
+
+        print(f"df создался за {time() - st} секунд")
 
 
 def main():
-    input_matrix_path = "data/input_matrix4.csv"
+    input_matrix_path = "data/input_matrix3.csv"
     out_path = "data/out.csv"
-    weights_path = "data/weights4.csv"
-    comp_matrix_finder = CompatabilityMatrixFinder(input_matrix_path, weights_path, out_path)
+    weights_path = "data/weights3.csv"
+    pred_path = "data/pred4.csv"
+    comp_matrix_finder = CompatabilityMatrixFinder(input_matrix_path, weights_path, out_path, pred_path)
     comp_matrix_finder()
 
 
